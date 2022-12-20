c8a784d18b8d07c466595d174dbfe2e2fc9dd7d0
dylanw422 <71285357+dylanw422@users.noreply.github.com>
2022-12-20 15:58:34 -0600

    Add files via upload

--- index.js
+++ index.js
@@ -16,17 +16,14 @@ let cRemain = [squareOne, squareTwo, squareThree, squareFour, squareFive, square
 
 let comp;
 
-function checkForWin() {
-    if (squareOne.textContent === squareTwo.textContent === squareThree.textContent ||
+let checkForWin = (squareOne.textContent === squareTwo.textContent === squareThree.textContent ||
         squareOne.textContent === squareFive.textContent === squareNine.textContent ||
         squareOne.textContent === squareFour.textContent === squareSeven.textContent ||
         squareTwo.textContent === squareFive.textContent === squareEight.textContent ||
         squareThree.textContent === squareSix.textContent === squareNine.textContent ||
         squareThree.textContent == squareFive.tagName === squareSeven.textContent ||
-        squareFour.textContent === squareFive.textContent === squareSix.textContent) {
-            alert('GAME OVER!')
-        }
-}
+        squareFour.textContent === squareFive.textContent === squareSix.textContent ||
+        squareSeven.textContent === squareEight.textContent === squareNine.textContent)
 
 function EasyComputerPlay() {
     if (cRemain.length <= 1) {
@@ -40,13 +37,189 @@ function EasyComputerPlay() {
 
 function HardComputerPlay() {
     if (cRemain.length <= 1) {
-        return
+        return        
     } else {
         if (squareFive.textContent === '') {
             comp = squareFive;
             comp.textContent = 'O'
             cRemain = cRemain.filter(choice => choice !== comp)
         } else if (squareTwo.textContent === 'X' && squareFive.textContent == 'X' && squareEight.textContent === '') {
+            comp = squareEight;
+            comp.textContent = 'O'
+            cRemain = cRemain.filter(choice => choice !== comp)
+        } else if (squareFive.textContent === 'X' && squareSix.textContent === 'X' && squareFour.textContent === '') {
+            comp = squareFour;
+            comp.textContent = 'O'
+            cRemain = cRemain.filter(choice => choice !== comp)
+        } else if (squareFive.textContent === 'X' && squareSeven.textContent === 'X' && squareThree.textContent === '') {
+            comp = squareThree;
+            comp.textContent = 'O'
+            cRemain = cRemain.filter(choice => choice !== comp)
+        } else if (squareFive.textContent === 'X' && squareEight.textContent === 'X' && squareTwo.textContent === '') {
+            comp = squareTwo;
+            comp.textContent = 'O'
+            cRemain = cRemain.filter(choice => choice !== comp)
+        } else if (squareFive.textContent === 'X' && squareNine.textContent === 'X' && squareOne.textContent === '') {
+            comp = squareOne;
+            comp.textContent = 'O'
+            cRemain = cRemain.filter(choice => choice !== comp)
+        } else if (squareOne.textContent === 'X' && squareTwo.textContent === 'X' && squareThree.textContent === '') {
+            comp = squareThree;
+            comp.textContent = 'O'
+            cRemain = cRemain.filter(choice => choice !== comp)
+        } else if (squareOne.textContent === 'X' && squareFour.textContent === 'X' && squareSeven.textContent === '') {
+            comp = squareSeven;
+            comp.textContent = 'O'
+            cRemain = cRemain.filter(choice => choice !== comp)
+        } else if (squareOne.textContent === 'X' && squareFive.textContent === 'X' && squareNine.textContent === '') {
+            comp = squareNine;
+            comp.textContent = 'O'
+            cRemain = cRemain.filter(choice => choice !== comp)
+        } else if (squareTwo.textContent === 'X' && squareThree.textContent === 'X' && squareOne.textContent === '') {
+            comp = squareOne;
+            comp.textContent = 'O'
+            cRemain = cRemain.filter(choice => choice !== comp)
+        } else if (squareThree.textContent === 'X' && squareFive.textContent === 'X' && squareSeven.textContent === '') {
+            comp = squareSeven;
+            comp.textContent = 'O'
+            cRemain = cRemain.filter(choice => choice !== comp)
+        } else if (squareThree.textContent === 'X' && squareSix.textContent === 'X' && squareNine.textContent === '') {
+            comp = squareNine;
+            comp.textContent = 'O'
+            cRemain = cRemain.filter(choice => choice !== comp)
+        } else if (squareFour.textContent === 'X' && squareFive.textContent === 'X' && squareSix.textContent === '') {
+            comp = squareSix;
+            comp.textContent = 'O'
+            cRemain = cRemain.filter(choice => choice !== comp)
+        } else if (squareFour.textContent === 'X' && squareSeven.textContent === 'X' && squareOne.textContent === '') {
+            comp = squareOne;
+            comp.textContent = 'O'
+            cRemain = cRemain.filter(choice => choice !== comp)
+        } else if (squareSix.textContent === 'X' && squareNine.textContent === 'X' && squareThree.textContent === '') {
+            comp = squareThree;
+            comp.textContent = 'O'
+            cRemain = cRemain.filter(choice => choice !== comp)
+        } else if (squareSeven.textContent === 'X' && squareEight.textContent === 'X' && squareNine.textContent === '') {
+            comp = squareNine;
+            comp.textContent = 'O'
+            cRemain = cRemain.filter(choice => choice !== comp)
+        } else if (squareEight.textContent === 'X' && squareNine.textContent === 'X' && squareSeven.textContent === '') {
+            comp = squareSeven;
+            comp.textContent = 'O'
+            cRemain = cRemain.filter(choice => choice !== comp)
+        } else if (squareOne.textContent === 'X' && squareSeven.textContent === 'X' && squareFour.textContent === '') {
+            comp = squareFour;
+            comp.textContent = 'O'
+            cRemain = cRemain.filter(choice => choice !== comp)
+        } else if (squareOne.textContent === 'X' && squareThree.textContent === 'X' && squareTwo.textContent === '') {
+            comp = squareTwo;
+            comp.textContent = 'O'
+            cRemain = cRemain.filter(choice => choice !== comp)
+        } else if (squareNine.textContent === 'X' && squareThree.textContent === 'X' && squareSix.textContent === '') {
+            comp = squareSix;
+            comp.textContent = 'O'
+            cRemain = cRemain.filter(choice => choice !== comp)
+        } else if (squareNine.textContent === 'X' && squareSeven.textContent === 'X' && squareEight.textContent === '') {
+            comp = squareEight;
+            comp.textContent = 'O'
+            cRemain = cRemain.filter(choice => choice !== comp)
+        } else {
+            comp = cRemain[Math.floor(Math.random()*cRemain.length)]
+            comp.textContent = 'O'
+            cRemain = cRemain.filter(choice => choice !== comp)
+        }
+    }
+}
+
+function ImpossibleComputerPlay() {
+    if (cRemain.length <= 1) {
+        return        
+    } else {
+        if (squareFive.textContent === '') {
+            comp = squareFive;
+            comp.textContent = 'O'
+            cRemain = cRemain.filter(choice => choice !== comp)
+        } else if (squareTwo.textContent === 'O' && squareFive.textContent == 'O' && squareEight.textContent === '') { //Check For O's
+            comp = squareEight;
+            comp.textContent = 'O'
+            cRemain = cRemain.filter(choice => choice !== comp)
+        } else if (squareFive.textContent === 'O' && squareSix.textContent === 'O' && squareFour.textContent === '') {
+            comp = squareFour;
+            comp.textContent = 'O'
+            cRemain = cRemain.filter(choice => choice !== comp)
+        } else if (squareFive.textContent === 'O' && squareSeven.textContent === 'O' && squareThree.textContent === '') {
+            comp = squareThree;
+            comp.textContent = 'O'
+            cRemain = cRemain.filter(choice => choice !== comp)
+        } else if (squareFive.textContent === 'O' && squareEight.textContent === 'O' && squareTwo.textContent === '') {
+            comp = squareTwo;
+            comp.textContent = 'O'
+            cRemain = cRemain.filter(choice => choice !== comp)
+        } else if (squareFive.textContent === 'O' && squareNine.textContent === 'O' && squareOne.textContent === '') {
+            comp = squareOne;
+            comp.textContent = 'O'
+            cRemain = cRemain.filter(choice => choice !== comp)
+        } else if (squareOne.textContent === 'O' && squareTwo.textContent === 'O' && squareThree.textContent === '') {
+            comp = squareThree;
+            comp.textContent = 'O'
+            cRemain = cRemain.filter(choice => choice !== comp)
+        } else if (squareOne.textContent === 'O' && squareFour.textContent === 'O' && squareSeven.textContent === '') {
+            comp = squareSeven;
+            comp.textContent = 'O'
+            cRemain = cRemain.filter(choice => choice !== comp)
+        } else if (squareOne.textContent === 'O' && squareFive.textContent === 'O' && squareNine.textContent === '') {
+            comp = squareNine;
+            comp.textContent = 'O'
+            cRemain = cRemain.filter(choice => choice !== comp)
+        } else if (squareTwo.textContent === 'O' && squareThree.textContent === 'O' && squareOne.textContent === '') {
+            comp = squareOne;
+            comp.textContent = 'O'
+            cRemain = cRemain.filter(choice => choice !== comp)
+        } else if (squareThree.textContent === 'O' && squareFive.textContent === 'O' && squareSeven.textContent === '') {
+            comp = squareSeven;
+            comp.textContent = 'O'
+            cRemain = cRemain.filter(choice => choice !== comp)
+        } else if (squareThree.textContent === 'O' && squareSix.textContent === 'O' && squareNine.textContent === '') {
+            comp = squareNine;
+            comp.textContent = 'O'
+            cRemain = cRemain.filter(choice => choice !== comp)
+        } else if (squareFour.textContent === 'O' && squareFive.textContent === 'O' && squareSix.textContent === '') {
+            comp = squareSix;
+            comp.textContent = 'O'
+            cRemain = cRemain.filter(choice => choice !== comp)
+        } else if (squareFour.textContent === 'O' && squareSeven.textContent === 'O' && squareOne.textContent === '') {
+            comp = squareOne;
+            comp.textContent = 'O'
+            cRemain = cRemain.filter(choice => choice !== comp)
+        } else if (squareSix.textContent === 'O' && squareNine.textContent === 'O' && squareThree.textContent === '') {
+            comp = squareThree;
+            comp.textContent = 'O'
+            cRemain = cRemain.filter(choice => choice !== comp)
+        } else if (squareSeven.textContent === 'O' && squareEight.textContent === 'O' && squareNine.textContent === '') {
+            comp = squareNine;
+            comp.textContent = 'O'
+            cRemain = cRemain.filter(choice => choice !== comp)
+        } else if (squareEight.textContent === 'O' && squareNine.textContent === 'O' && squareSeven.textContent === '') {
+            comp = squareSeven;
+            comp.textContent = 'O'
+            cRemain = cRemain.filter(choice => choice !== comp)
+        } else if (squareOne.textContent === 'O' && squareSeven.textContent === 'O' && squareFour.textContent === '') {
+            comp = squareFour;
+            comp.textContent = 'O'
+            cRemain = cRemain.filter(choice => choice !== comp)
+        } else if (squareOne.textContent === 'O' && squareThree.textContent === 'O' && squareTwo.textContent === '') {
+            comp = squareTwo;
+            comp.textContent = 'O'
+            cRemain = cRemain.filter(choice => choice !== comp)
+        } else if (squareNine.textContent === 'O' && squareThree.textContent === 'O' && squareSix.textContent === '') {
+            comp = squareSix;
+            comp.textContent = 'O'
+            cRemain = cRemain.filter(choice => choice !== comp)
+        } else if (squareNine.textContent === 'O' && squareSeven.textContent === 'O' && squareEight.textContent === '') {
+            comp = squareEight;
+            comp.textContent = 'O'
+            cRemain = cRemain.filter(choice => choice !== comp)
+        } else if (squareTwo.textContent === 'X' && squareFive.textContent == 'X' && squareEight.textContent === '') { //Check For X's
             comp = squareEight;
             comp.textContent = 'O'
             cRemain = cRemain.filter(choice => choice !== comp)
@@ -147,6 +320,10 @@ for (let i=0; i<choices.length; i++) {
             choices[i].textContent = 'X'
             cRemain = cRemain.filter(choice => choice !== choices[i])
             HardComputerPlay()
+        } else if (level.value === 'Impossible') {
+            choices[i].textContent = 'X'
+            cRemain = cRemain.filter(choice => choice !== choices[i])
+            ImpossibleComputerPlay()
         }
     })
 }
